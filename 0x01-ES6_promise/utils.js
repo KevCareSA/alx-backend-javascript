a88cd017a647f76e5b46250871737fa5f9e84a11
KevCareSA <kev.mkbn2@gmail.com>
2024-01-03 18:08:54 +0200

    promises tasks

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,14 @@
+// utils.js
+const uploadPhoto = () =>
+// Implement the uploadPhoto function logic
+  ({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+const createUser = () =>
+// Implement the createUser function logic
+  ({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+module.exports = { uploadPhoto, createUser };
